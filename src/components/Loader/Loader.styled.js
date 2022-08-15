348de93de83595e4a853b1d5a16ebed22e9f0478
Nazarii0504 <91625478+Nazarii0504@users.noreply.github.com>
2022-08-15 19:17:06 +0300

    Update Loader.styled.js
    add

--- src/components/Loader/Loader.styled.js
+++ src/components/Loader/Loader.styled.js
@@ -1,7 +1,8 @@
 import styled from 'styled-components';
 
 export const LoaderContainer = styled.div`
-  width: 50px;
-  display: block;
-  margin: 0 auto;
+  position: fixed;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
 `;
