e845f91c4fe93adb7cc8c572a1ee31f07ffe4153
Nazarii0504 <91625478+Nazarii0504@users.noreply.github.com>
2022-08-15 19:15:25 +0300

    Update Searchbar.jsx
    add

--- src/components/Searchbar/Searchbar.jsx
+++ src/components/Searchbar/Searchbar.jsx
@@ -1,3 +1,4 @@
+import PropTypes from 'prop-types';
 import { Formik } from 'formik';
 import { toast } from 'react-toastify';
 // import { ImSearch } from 'react-icons/im';
@@ -28,4 +29,7 @@ export const Searchbar = ({ onSubmit }) => {
       </Formik>
     </SearchBarheader>
   );
+};
+PropTypes.Searchbar = {
+  onSubmit: PropTypes.func.isRequired,
 };
